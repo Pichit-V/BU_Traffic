1ce90fca3cf93662cd2ae2281aae6aed959c7265
Pichit <pichit.v@bu.ac.th>
2015-11-14 16:02:14 +0700

    Finish 14 Nov 58

--- app/src/main/java/bu/pichit/butraffic/MainActivity.java
+++ app/src/main/java/bu/pichit/butraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package bu.pichit.butraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -42,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
 
 
                // Intent to WebView
+               Intent objIntent = new Intent(Intent.ACTION_VIEW);
+               objIntent.setData(Uri.parse("http://androidthai.in.th/"));
+               startActivity(objIntent);
+
 
            }
        });
